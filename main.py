aa593d97f281a43b193d3dc312d4878bc4f3edb6
Kennari Brown <kbrown256@hawkmail.hccfl.edu>
2025-04-19 14:01:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ This program calculates prices for custom house signs.
 # Color of characters.
 # Type of wood.
 
+charge = 0.00
+numChars = 18
+color = "black"
+woodType = "oak"
+
+charge = 35.00
+
 # Write assignment and if statements here as appropriate.
 
+if numChars > 5:
+    charge += 4 * (numChars - 5)
+
+if woodType == "oak":
+    charge += 20.00
+
+if color == "gold":
+    charge += 15.00
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
+
